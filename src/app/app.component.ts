8b78e32c1e3559fe58fa529977acb72d2989d096
daliyajohn <daliyajohn75@gmail.com>
2018-12-08 22:36:59 +0530

    Fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,18 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  value = '';
-
-  constructor() {}
-
-  onEnter(value, event) {
-    this.value = value;
-
-    let position = event.path['0'].childNodes.length;
-    var g = document.createElement('div');
-    g.setAttribute('id', 'Div' + position);
-    // var textnode = document.createTextNode(value);
-    // g.appendChild(textnode);
-    document.getElementById('myList').appendChild(g);
+  constructor() {
+    var div = document.querySelector('div');
+    console.log(div);
   }
 }
